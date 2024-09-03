937fe8f17857cf64e8dc0ae1f99ed9c71f9473a1
kaiiruu <120348434+kaiiruu@users.noreply.github.com>
2024-09-03 11:30:43 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE YOU, TRIXIA <3').split('')
+    const titles = ("I miss you, Don't you miss me?").split("")
     const titleElement = document.getElementById('title');
     let index = 0;
 
