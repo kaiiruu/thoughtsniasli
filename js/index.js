2791cb7eaca9a21c673d8d56316c378d3132c4c7
kaiiruu <120348434+kaiiruu@users.noreply.github.com>
2024-09-03 11:29:30 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'HEY BABY!!!'.split('')
+const text = 'Click if you miss me'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
